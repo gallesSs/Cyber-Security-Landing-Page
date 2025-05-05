2a0255a4cf4c9ae0104b241c300acd72ee5db890
Arthur <agavrik464@gmail.com>
2025-05-05 11:59:32 +0300

    Header done

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import s from "./Hero.module.css"
+import {IoSearch} from "react-icons/io5";
+
+
+const Hero = () => {
+	return (
+		<section className={s.hero}>
+			<div className={s.container}>
+				<img className={s.img} src="/Hero/Hero.png" alt="hero"/>
+				<span className={s.upText}>Ensuring maximum security</span>
+				<h1 className={s.headerText}>Protect all your data with strong security access</h1>
+				<p className={s.info}>This is software that protects all your data, including strong security access. Use data
+					as
+					needed and provide
+					security.</p>
+				<button className={s.btn}><IoSearch className={s.icon}/><span
+					className={s.btnText}>Scan Your Website - Free!</span></button>
+			</div>
+
+		</section>
+	);
+};
+
+export default Hero;
